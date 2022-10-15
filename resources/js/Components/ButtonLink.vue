237ff4e47e55fb25f917bf97685f54ccdf9b956a
YuNandarMoe31 <yunandarmoe31@gmail.com>
2022-10-15 14:44:02 +0630

    Table Refactoring

---
+++ resources/js/Components/ButtonLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <Link :href="link"
+        class="px-4 py-2 rounded-lg bg-green-500 hover:bg-green-700 text-white"
+    >
+        <slot />
+    </Link>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { Link } from "@inertiajs/inertia-vue3";
+
+defineProps({
+    link: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
