ccc660f254ecb7b266650c12ddbcf1588561d036
YuNandarMoe31 <yunandarmoe31@gmail.com>
2022-10-18 20:29:35 +0630

    Frontend Header & Footer

---
+++ resources/js/Layouts/FrontLayout.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+import Footer from '../Components/Footer.vue'
+import Navbar from '@/Components/Navbar.vue'
+import { ref } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    title: String,
+});
+
+</script>
+
+<template>
+    <div>
+        <Head :title="title" />
+
+        <!-- Nav -->
+        <Navbar />
+
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>           
+        </div>
+
+        <!-- Footer -->
+        <Footer />
+    </div>
+</template>
